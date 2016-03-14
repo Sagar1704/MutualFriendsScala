e4eff568087e2c4babc0a86edecae7b59afe76f0
Sagar Deshpande <deshpandesagar1704@gmail.com>
2016-03-14 02:20:29 -0500

    Changed the filter to search the users

--- src/main/scala/Mutual.scala
+++ src/main/scala/Mutual.scala
@@ -2,6 +2,7 @@ package main.scala
 
 import org.apache.spark.SparkConf
 import org.apache.spark.SparkContext
+import scala.util.matching.Regex
 
 object Mutual {
     def main(args: Array[String]): Unit = {
@@ -12,8 +13,8 @@ object Mutual {
         val userB = args(2)
 
         val input = sc.textFile(args(0));
-        val userAFriends = input.filter(line => line.contains(userA + "\t")).flatMap(line => line.split("\t")(1).split(","))
-        val userBFriends = input.filter(line => line.contains(userB + "\t")).flatMap(line => line.split("\t")(1).split(","))
+        val userAFriends = input.filter(line => line.startsWith(userA + "\t")).flatMap(line => line.split("\t")(1).split(","))
+        val userBFriends = input.filter(line => line.startsWith(userB + "\t")).flatMap(line => line.split("\t")(1).split(","))
 
         val unionOfFriends = userAFriends.union(userBFriends)
         val mutualFriends = unionOfFriends.map(friend => (friend, 1)).reduceByKey(_ + _).filter(friendCount => (friendCount._2 == 2)).map(friendCount => friendCount.swap).reduceByKey(_ + "," + _).map(countFriend => (userA + "," + userB + "\t" + countFriend._2))
